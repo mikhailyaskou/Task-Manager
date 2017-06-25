dbdf7020b0c0610f87122ff6f98e8f00c0cb3bf2
Mikhail Yaskou <mikhailyaskow@gmail.com>
2017-06-25 12:12:04 +0300

    Commit before refactoring search view

---
+++ Task Manager/YMABasicViewController.h
@@ -0,0 +1,13 @@
+//
+//  YMABasicViewController.h
+//  Task Manager
+//
+//  Created by Mikhail Yaskou on 23.06.17.
+//  Copyright © 2017 Mikhail Yaskou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMABasicViewController : UIViewController
+
+@end
